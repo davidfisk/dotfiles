8916785cd62baf8ad090c0020810656788ba2d9d
davidfisk <mail@davidfisk.com>
2026-08-10 19:28:07 -0700

    rm garbage and add pico 8

--- conf/nvim/lua/dfisk/pack/nvim-lspconfig.lua
+++ conf/nvim/lua/dfisk/pack/nvim-lspconfig.lua
@@ -6,7 +6,6 @@ cmp.setup {
   -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
   -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
   keymap = { preset = 'default' },
-
   appearance = {
     -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- Adjusts spacing to ensure icons are aligned
@@ -176,7 +175,7 @@ local servers = {
   html = {},
   lua_ls = {
     -- cmd = {...},
-    filetypes = { 'lua' },
+    filetypes = { 'lua', 'p8' },
     -- capabilities = {},
     settings = {
       Lua = {
--- conf/nvim/lua/dfisk/vim_opts.lua
+++ conf/nvim/lua/dfisk/vim_opts.lua
@@ -124,3 +124,5 @@ for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
+
+vim.lsp.codelens.enable(true)
