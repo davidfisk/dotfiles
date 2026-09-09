71ee9a08b3ba624c83826879aa7de879e4794e46
David <mail@davidfisk.com>
2026-09-09 10:54:53 -0700

    linux updates

--- conf/nvim/init.lua
+++ conf/nvim/init.lua
@@ -1,6 +1,12 @@
 require 'dfisk.vim_opts'
 require 'dfisk.vim_keymaps'
 require 'dfisk.vim_autocmd'
--- require 'dfisk.lazy'
 require 'dfisk.init'
 require 'dfisk.vim_post_opts'
+
+-- use pack if nvim > 0.12
+if vim.fn.has 'nvim-0.12' == 1 then
+  require 'dfisk.init'
+else
+  require 'dfisk.lazy'
+end
